2f018aeef50c32875fda67b762b428d3c9bb0e21
domemp <douglasamaral.emp@gmail.com>
2024-03-29 15:58:47 -0300

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import '@/app/ui/global.css';
+import { nunito } from '@/app/ui/fonts';
 
 export default function RootLayout({
   children,
@@ -7,7 +8,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <title>Stark - Stark</title>
+      <body className={`${nunito.className} antialiased`} >{children}</body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Nunito_Sans } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+
+export const nunito = Nunito_Sans({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
